bbc03ca41c402252b1e8e8c88ef92039f4c2d030
alfres <alfre_alfree@yahoo.es>
2017-01-04 18:19:21 +0100

    Delete llenguas.js

--- listas/llenguas.js
+++
@@ -1,4 +0,0 @@
- var listlenguas = [
- ["SPANISH","ENGLISH","FRANCAIS","EUSKERA","DEUTSCH","SOUFFLE"],
-["Spanish Female","UK English Female","French Female","Serbo-Croatian Male","Deutsch Female"]
-]
