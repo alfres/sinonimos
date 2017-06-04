0e23046cdc0601ccbbded0ad9d6f1d7025c7a48a
alfres <alfre_alfree@yahoo.es>
2017-06-04 19:32:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static('public'))
 
 app.get('/', function(request, response) {
-  response.sendFile( __dirname + "/" + "index.html" );
+  response.sendFile( "index.html" );
 })
 
 app.listen(app.get('port'), function() {
