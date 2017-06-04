85f5c3f66b61d83a735d99ea015ad0c346ae7e14
alfres <alfre_alfree@yahoo.es>
2017-06-04 18:55:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-
+app.use(express.static('public'));
 
 app.get('/index.html', function(request, response) {
   response.sendFile( __dirname + "/" + "index.htm" );
